86a084badc2a452c2bdf995bf01e4614ef1511ea
Matheus Galvao Correia <matheus.nearsure@impel.io>
2023-05-24 10:42:31 -0300

    feat: sales history etl class

--- carlabs/app/etl.py
+++ carlabs/app/etl.py
@@ -6,6 +6,9 @@ from orm.models.carlabs import DataImports
 from orm.models.shared_dms import Vehicle, VehicleSale, Consumer, ServiceContract
 from transformers.sales_history import CDKTransformer, DealertrackTransformer, DealervaultTransformer
 from dataclasses import dataclass
+import logging
+
+logger = logging.getLogger()
 
 
 class DMSNotMapped(Exception):
@@ -13,85 +16,90 @@ class DMSNotMapped(Exception):
 
 
 @dataclass
-class TransformedData:
+class SalesHistoryTransformedData:
     vehicle_sale: VehicleSale
     vehicle: Vehicle
     consumer: Consumer
     service_contract: ServiceContract
 
 
-def extract_from_carlabs(limit: int = 100):
-    engine = db.create_engine(make_db_uri(db='CARLABS'))
-    df = pandas.read_sql_query(
-        sql=db.select(DataImports).where(
-            (DataImports.data_type == 'SALES')
-        ).limit(limit),
-        con=engine)
-    breakpoint()
+@dataclass
+class SalesHistoryETLProcess:
 
-    for r in df.to_dict('records'):
-        if isinstance(r['importedData'], list):
-            for i in r['importedData']:
-                yield {
-                    'id': r['id'],
-                    'dealerCode': r['dealerCode'],
-                    'importedData': i,
-                    'dataSource': r['dataSource'],
-                    'dataType': r['dataType'],
-                    'creationDate': r['creationDate']
-                }
+    limit: int
+    offset: int
+
+
+    def __extract_from_carlabs(self):
+        engine = db.create_engine(make_db_uri(db='CARLABS'))
+        df = pandas.read_sql_query(
+            sql=db.select(DataImports).where(
+                (DataImports.data_type == 'SALES')
+            ).limit(self.limit).offset(self.offset),
+            con=engine)
+        breakpoint()
+
+        for r in df.to_dict('records'):
+            if isinstance(r['importedData'], list):
+                for i in r['importedData']:
+                    yield {
+                        'id': r['id'],
+                        'dealerCode': r['dealerCode'],
+                        'importedData': i,
+                        'dataSource': r['dataSource'],
+                        'dataType': r['dataType'],
+                        'creationDate': r['creationDate']
+                    }
+            else:
+                yield r
+
+
+    def __transform(self, record: dict):
+        data_source = record['dataSource']
+        if data_source == 'CDK':
+            transformer = CDKTransformer(carlabs_data=record)
+        elif data_source == 'DealerTrack':
+            transformer = DealertrackTransformer(carlabs_data=record)
+        elif data_source == 'DEALERVAULT':
+            transformer = DealervaultTransformer(carlabs_data=record)
         else:
-            yield r
+            raise DMSNotMapped
+
+        return SalesHistoryTransformedData(
+            vehicle_sale=transformer.vehicle_sale,
+            vehicle=transformer.vehicle,
+            consumer=transformer.consumer,
+            service_contract=transformer.service_contract
+        )
 
 
-def transform(record: dict):
-    data_source = record['dataSource']
-    if data_source == 'CDK':
-        transformer = CDKTransformer(carlabs_data=record)
-    elif data_source == 'DealerTrack':
-        transformer = DealertrackTransformer(carlabs_data=record)
-    elif data_source == 'DEALERVAULT':
-        transformer = DealervaultTransformer(carlabs_data=record)
-    else:
-        raise DMSNotMapped
+    def __load_into_shared_dms(self, transformed: SalesHistoryTransformedData):
+        transformed.vehicle_sale.vehicle = transformed.vehicle
+        transformed.vehicle_sale.consumer = transformed.consumer
 
-    return TransformedData(
-        vehicle_sale=transformer.vehicle_sale,
-        vehicle=transformer.vehicle,
-        consumer=transformer.consumer,
-        service_contract=transformer.service_contract
-    )
+        transformed.service_contract.vehicle = transformed.vehicle
+        transformed.service_contract.consumer = transformed.consumer
+        transformed.service_contract.sale = transformed.vehicle_sale
+
+        with SQLSession(db='SHARED_DMS') as session:
+            session.add(transformed.vehicle)
+            session.add(transformed.consumer)
+            session.add(transformed.vehicle_sale)
+            if transformed.vehicle_sale.has_service_contract:
+                session.add(transformed.service_contract)
 
 
-def load_into_shared_dms(transformed: TransformedData):
-    transformed.vehicle_sale.vehicle = transformed.vehicle
-    transformed.vehicle_sale.consumer = transformed.consumer
+    def run(self):
+        records = self.__extract_from_carlabs()
+        n_transformed = 0
+        failed = 0
 
-    transformed.service_contract.vehicle = transformed.vehicle
-    transformed.service_contract.consumer = transformed.consumer
-    transformed.service_contract.sale = transformed.vehicle_sale
+        for r in records:
+            try:
+                self.__load_into_shared_dms(self.__transform(r))
+                n_transformed += 1
+            except Exception:
+                logger.exception(f'failed to transform {r["id"]}')
+                failed += 1
 
-    with SQLSession(db='SHARED_DMS') as session:
-        session.add(transformed.vehicle)
-        session.add(transformed.consumer)
-        session.add(transformed.vehicle_sale)
-        if transformed.vehicle_sale.has_service_contract:
-            session.add(transformed.service_contract)
-
-
-import logging
-logger = logging.getLogger()
-
-records = extract_from_carlabs()
-n_transformed = 0
-failed = 0
-
-for r in records:
-    try:
-        load_into_shared_dms(transform(r))
-        n_transformed += 1
-    except Exception as e:
-        logger.exception(f'failed to transform {r["id"]}')
-        failed += 1
-
-print(f'loaded {n_transformed}, failed {failed}')
+        print(f'loaded {n_transformed}, failed {failed}')
