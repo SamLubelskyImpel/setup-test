54338720291f9eb92b6540d2bbd347dea3e31117
Matheus Galvao Correia <matheus.nearsure@impel.io>
2023-06-15 14:25:16 -0300

    fix: repair order comment

--- carlabs/app/mapping/repair_order/repair_order.py
+++ carlabs/app/mapping/repair_order/repair_order.py
@@ -9,8 +9,7 @@ def map_service_repair_order(record: RepairOrder, dip_id: int):
     ro.txn_pay_type = 'warranty' if record.warranty_flag else 'consumer'
     ro.repair_order_no = record.ro_number
     ro.total_amount = record.total_amount
-    # TODO this field needs to be of type  TEXT
-    # ro.comment = str(record.ro_service_details)
+    ro.comment = str(record.ro_service_details)
     ro.dealer_integration_partner_id = dip_id
     ro.metadata_column = {'repair_order_id': record.id, 'data_source': record.ro_source}
     return ro
