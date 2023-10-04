2694c506290c807619e2d02f9a2b52afafea4ddf
etarasovaspincar <elena@spincar.com>
2023-10-04 14:27:31 -0400

    added template and authorizer

---
+++ crm-api/app/create_consumer.py
@@ -0,0 +1,10 @@
+import logging
+from os import environ
+
+logger = logging.getLogger()
+logger.setLevel(environ.get("LOGLEVEL", "INFO").upper())
+
+
+def lambda_handler(event, context):
+    """Create consumer."""
+    logger.info(f"Event: {event}")
