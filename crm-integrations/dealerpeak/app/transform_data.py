b61a89e45862dba557df6edf4e6c4a9dd0342c34
etarasovaspincar <elena@spincar.com>
2023-11-13 09:45:12 -0500

    added Eventbridge

--- crm-integrations/dealerpeak/app/transform_data.py
+++ crm-integrations/dealerpeak/app/transform_data.py
@@ -3,30 +3,36 @@
 import boto3
 import logging
 from os import environ
-from json import dumps, loads
+from json import loads
 from typing import Any
-from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSRecord
-from aws_lambda_powertools.utilities.batch import (
-    BatchProcessor,
-    EventType,
-    process_partial_response,
-)
 
 
 logger = logging.getLogger()
 logger.setLevel(environ.get("LOGLEVEL", "INFO").upper())
 
 
-def record_handler(record: SQSRecord) -> Any:
-    """Process individual SQS record."""
-    logger.info(f"Record: {record}")
+s3_client = boto3.client('s3')
 
-    try:
-        body = loads(record['body'])
-        logger.info(f"Data: {body}")
-    except Exception as e:
-        logger.error(f"Error processing record: {e}")
-        raise
+
+# def record_handler() -> Any:
+#     """Process individual SQS record."""
+#     logger.info(f"Record: {record}")
+
+#     try:
+
+#         # Process json_data as needed
+
+#         for item in json_data:
+#             salesperson = json_data.get('agent', None)
+#             salesperson_id = salesperson.get('userID', None)
+#             salesperson_first_name = salesperson.get('givenName', None)
+#             salesperson_last_name = salesperson.get('familyName', None)
+#             salesperson_emails = salesperson.get('contactInformation', None).get('emails', None)
+#             logger.info(salesperson_id, salesperson_first_name, salesperson_last_name, salesperson_emails)  
+
+#     except Exception as e:
+#         logger.error(f"Error processing record: {e}")
+#         raise
 
 
 def lambda_handler(event: Any, context: Any) -> Any:
@@ -34,16 +40,15 @@ def lambda_handler(event: Any, context: Any) -> Any:
     logger.info(f"Event: {event}")
 
     try:
-        processor = BatchProcessor(event_type=EventType.SQS)
-        result = process_partial_response(
-            event=event,
-            record_handler=record_handler,
-            processor=processor,
-            context=context
-        )
-
-        return result
-
-    except Exception as e:
-        logger.error(f"Error processing batch: {e}")
+        for record in event["Records"]:
+            message = loads(record["body"])
+            for s3_record in message["Records"]:
+                bucket = s3_record["s3"]["bucket"]["name"]
+                key = s3_record["s3"]["object"]["key"]
+                response = s3_client.get_object(Bucket=bucket, Key=key)
+                content = response['Body'].read()
+                json_data = loads(content)
+                logger.info(f"Data: {json_data}")
+    except Exception:
+        logger.exception(f"Error transforming dealerpeak file {event}")
         raise
