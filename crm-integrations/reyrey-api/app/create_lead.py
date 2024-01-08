4d23478ae010c4877c01d1ab59c025f62d416718
Luka Esakia <lesakia@impel.io>
2024-01-08 19:41:07 +0400

    init POST create leads endpoint, init XML transformation

--- crm-integrations/reyrey-api/app/create_lead.py
+++ crm-integrations/reyrey-api/app/create_lead.py
@@ -1,7 +1,90 @@
+import logging
+import boto3
+import json
+import xml.etree.ElementTree as ET
+from datetime import datetime
+from shared_resources.app.get_active_dealers import get_dealers
+from os import environ
 from typing import Any
+from uuid import uuid4
+
+BUCKET = environ.get("INTEGRATIONS_BUCKET")
+
+logger = logging.getLogger()
+logger.setLevel(environ.get("LOGLEVEL", "INFO").upper())
+s3_client = boto3.client("s3")
 
 
 def lambda_handler(event: Any, context: Any) -> Any:
-    return {
-        "statusCode": 200
-    }
+    """This API handler takes the XML sent by ReyRey and puts the raw XML into the S3 bucket."""
+    try:
+        logger.info(f"Event: {event}")
+
+        lead_xml_body = event["body"]
+        reyrey_dealer_list = get_dealers("reyrey_crm")
+        crm_dealer_id = extract_crm_dealer_id(lead_xml_body)
+        product_dealer_id = None
+
+        for dealer in reyrey_dealer_list:
+            if dealer["crm_dealer_id"] == crm_dealer_id:
+                product_dealer_id = dealer["product_dealer_id"]
+                break
+
+        if not product_dealer_id:
+            logger.error(f"Dealer {crm_dealer_id} not found in active dealers.")
+            return {
+                "statusCode": 401,
+                "body": {
+                    "error": "This request is unauthorized. The authorization credentials are missing or are wrong. For example if the partner_id or the x_api_key provided in the header are wrong/missing. This error can also occur if the dealer_id provided hasn't been configured with Impel."
+                },
+            }
+
+        logger.info("New lead received for dealer: " + product_dealer_id)
+
+        save_raw_leads(lead_xml_body, product_dealer_id)
+
+        return {"statusCode": 200, "body": "Success."}
+    except ValueError as e:
+        return {"statusCode": 400, "body": json.dumps({"error": str(e)})}
+    except Exception as e:
+        logger.error(f"Error creating consumer: {str(e)}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps(
+                {"error": "Internal Server Error. Please contact Impel support."}
+            ),
+        }
+
+
+def save_raw_leads(lead: str, product_dealer_id: str):
+    """Save raw leads to S3."""
+    format_string = "%Y/%m/%d/%H/%M"
+    date_key = datetime.utcnow().strftime(format_string)
+
+    s3_key = f"raw/reyrey_crm/{product_dealer_id}/{date_key}_{uuid4()}.json"
+    logger.info(f"Saving reyrey lead to {s3_key}")
+    s3_client.put_object(
+        Body=lead,
+        Bucket=BUCKET,
+        Key=s3_key,
+    )
+
+
+def extract_crm_dealer_id(lead_xml_body: str) -> str:
+    """Extract CRM dealer ID from incoming xml."""
+    root = ET.fromstring(lead_xml_body)
+    if (
+        root.tag
+        != "{http://www.starstandards.org/STAR}rey_ImpelCRMPublishLeadDisposition"
+    ):
+        raise ValueError("Invalid XML format")
+
+    namespace = {"star": "http://www.starstandards.org/STAR"}
+
+    dealer_number = root.find(".//star:DealerNumber", namespace).text
+    store_number = root.find(".//star:StoreNumber", namespace).text
+    area_number = root.find(".//star:AreaNumber", namespace).text
+
+    concatenated_dealer_id = f"{dealer_number}_{store_number}_{area_number}"
+
+    return concatenated_dealer_id
---
+++ crm-integrations/reyrey/app/transform_new_lead.py
@@ -0,0 +1,252 @@
+"""Transform raw ReyRey_CRM data into the Unified Format."""
+
+import boto3
+import logging
+import requests
+import json
+import xml.etree.ElementTree as ET
+from os import environ
+from typing import Any, Dict
+from datetime import datetime
+from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSRecord
+from aws_lambda_powertools.utilities.batch import (
+    SqsFifoPartialProcessor,
+    process_partial_response,
+)
+
+
+logger = logging.getLogger()
+logger.setLevel(environ.get("LOG_LEVEL", "INFO").upper())
+
+ENVIRONMENT = environ.get("ENVIRONMENT")
+CRM_API_DOMAIN = environ.get("CRM_API_DOMAIN")
+UPLOAD_SECRET_KEY = environ.get("UPLOAD_SECRET_KEY")
+DA_SECRET_KEY = environ.get("DA_SECRET_KEY")
+SNS_TOPIC_ARN = environ.get("SNS_TOPIC_ARN")
+
+sm_client = boto3.client("secretsmanager")
+s3_client = boto3.client("s3")
+
+
+# def post_and_forward_entry(
+#     entry: dict, crm_api_key: str, listener_secrets: dict, index: int
+# ) -> bool:
+#     """Process a single entry."""
+#     logger.info(f"[THREAD {index}] Processing entry {entry}")
+#     try:
+#         lead_id = upload_entry_to_db(entry=entry, api_key=crm_api_key, index=index)
+#         data = {
+#             "message": "New Lead available from CRM API",
+#             "lead_id": lead_id,
+#         }
+#         logger.info(f"[THREAD {index}] Sending data to DA Event Listener: {data}")
+#         send_to_event_listener(
+#             data=data, listener_secrets=listener_secrets, index=index
+#         )
+#     except EventListenerError as e:
+#         send_alert_notification(e, lead_id)
+#     except Exception as e:
+#         if "409" in str(e):
+#             # Log the 409 error and continue with the next entry
+#             logger.warning(f"[THREAD {index}] {e}")
+#         else:
+#             logger.error(f"[THREAD {index}] Error uploading entry to DB: {e}")
+#             return False
+
+#     return True
+
+def get_secret(secret_name, secret_key) -> Any:
+    """Get secret from Secrets Manager."""
+    secret = sm_client.get_secret_value(
+        SecretId=f"{'prod' if ENVIRONMENT == 'prod' else 'test'}/{secret_name}"
+    )
+    secret = json.loads(secret["SecretString"])[str(secret_key)]
+    secret_data = json.loads(secret)
+
+    return secret_data
+
+def extract_consumer(root: ET.Element, namespace: dict) -> dict:
+    """Extract consumer data from the XML."""
+    name_rec_id = root.find(".//star:NameRecId", namespace).text
+    first_name = root.find(".//star:FirstName", namespace).text
+    last_name = root.find(".//star:LastName", namespace).text
+    email_mail_to = root.find(".//star:Email/star:MailTo", namespace).text
+    phone_num = root.find(".//star:PhoneNumbers/star:Phone/star:Num", namespace).text
+    consent_email = root.find(".//star:Consent/star:Email", namespace).text
+    consent_text = root.find(".//star:Consent/star:Text", namespace).text
+
+    # Assemble the payload for the CRM API
+    extracted_data = {
+        "crm_consumer_id": name_rec_id,
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email_mail_to,
+        "phone": phone_num,
+        "email_optin_flag": True if consent_email == "Y" else False,
+        "sms_optin_flag": True if consent_text == "Y" else False,
+    }
+    return extracted_data
+
+
+def extract_lead(root: ET.Element, namespace: dict) -> dict:
+    """Extract lead, vehicle of interest, salesperson data from the XML."""
+
+    # Extract Prospect fields
+    prospect_id = root.find(".//star:ProspectId", namespace).text
+    inserted_by = root.find(".//star:InsertedBy", namespace).text
+    prospect_status_type = root.find(".//star:ProspectStatusType", namespace).text
+    prospect_note = root.find(".//star:ProspectNote", namespace).text
+    prospect_type = root.find(".//star:ProspectType", namespace).text
+
+    prospect_data = {
+        "crm_lead_id": prospect_id,
+        "lead_ts": inserted_by,
+        "lead_status": prospect_status_type,
+        "lead_substatus": None,
+        "lead_comment": prospect_note,
+        "lead_origin": prospect_type,
+        "lead_source": None,
+    }
+
+    # Extract Vehicle of Interest fields
+    vin = root.find(".//star:DesiredVehicle/star:Vin", namespace).text
+    stock_id = root.find(".//star:DesiredVehicle/star:StockId", namespace).text
+    vehicle_make = root.find(".//star:DesiredVehicle/star:VehicleMake", namespace).text
+    vehicle_model = root.find(
+        ".//star:DesiredVehicle/star:VehicleModel", namespace
+    ).text
+    vehicle_year = root.find(".//star:DesiredVehicle/star:VehicleYear", namespace).text
+    vehicle_style = root.find(
+        ".//star:DesiredVehicle/star:VehicleStyle", namespace
+    ).text
+    stock_type = root.find(".//star:DesiredVehicle/star:StockType", namespace).text
+
+    vehicle_of_interest_data = {
+        "vin": vin,
+        "stock_number": stock_id,
+        "make": vehicle_make,
+        "oem_name": vehicle_make,
+        "model": vehicle_model,
+        "year": vehicle_year,
+        "type": vehicle_style,
+        "body_style": vehicle_style,
+        "condition": stock_type,
+        "class": None,
+        "mileage": None,
+        "trim": None,
+        "transmission": None,
+        "interior_color": None,
+        "exterior_color": None,
+        "price": None,
+        "status": None,
+        "odometer_units": None,
+        "vehicle_comments": None,
+    }
+
+    # Extract Salesperson fields, primary salesperson format: "Last, First"
+    primary_salesperson = root.find(
+        ".//star:Record/star:Prospect/star:PrimarySalesPerson", namespace
+    ).text
+    first_name = primary_salesperson.split(",")[1].strip()
+    last_name = primary_salesperson.split(",")[0].strip()
+    salesperson_data = {
+        "crm_salesperson_id": primary_salesperson,
+        "first_name": first_name,
+        "last_name": last_name,
+        "is_primary": True,
+        "position_name": "Primary Salesperson",
+        "email": None,
+        "phone": None,
+    }
+
+    # Add Vehicle of Interest and Salesperson data to the Prospect data
+    prospect_data["vehicle_of_interest"] = vehicle_of_interest_data
+    prospect_data["salesperson"] = salesperson_data
+
+    return prospect_data
+
+
+def record_handler(record: SQSRecord) -> None:
+    """Transform and process each record."""
+    logger.info(f"Record: {record}")
+    try:
+        message = json.loads(record["body"])
+        bucket = message["detail"]["bucket"]["name"]
+        key = message["detail"]["object"]["key"]
+        product_dealer_id = key.split("/")[2]
+
+        response = s3_client.get_object(Bucket=bucket, Key=key)
+        content = response["Body"].read()
+        xml_data = content
+        logger.info(f"Raw data: {xml_data}")
+        crm_api_key = get_secret(secret_name="crm-api", secret_key=UPLOAD_SECRET_KEY)[
+            "api_key"
+        ]
+
+        root = ET.fromstring(xml_data)
+        namespace = {"star": "http://www.starstandards.org/STAR"}
+
+        # Extract consumer data and write it to the Unified Layer
+        consumer = extract_consumer(root, namespace)
+        unified_crm_consumer_id = (
+            requests.post(
+                f"https://{CRM_API_DOMAIN}/consumers",
+                json=consumer,
+                headers={"x-api-key": crm_api_key},
+            )
+            .json()
+            .get("consumer_id")
+        )
+
+        if not unified_crm_consumer_id:
+            logger.error(f"Error creating consumer: {consumer}")
+            # TODO Check if this should be any other time of Exception
+            raise Exception("Error creating consumer")
+
+        # Extract lead data and write it to the Unified Layer, using the received consumer_id assigned by the layer
+        lead = extract_lead(root, namespace)
+        lead["consumer_id"] = unified_crm_consumer_id
+
+        unified_crm_lead_id = (
+            requests.post(
+                f"https://{CRM_API_DOMAIN}/leads",
+                json=lead,
+                headers={"x-api-key": crm_api_key},
+            )
+            .json()
+            .get("lead_id")
+        )
+
+        if not unified_crm_lead_id:
+            logger.error(f"Error creating lead: {lead}")
+            # TODO Check if this should be any other time of Exception
+            raise Exception("Error creating lead")
+
+        # logger.info(f"Transformed entries: {entries}")
+
+        event_listener_secrets = get_secret(
+            secret_name="crm-integrations-partner", secret_key=DA_SECRET_KEY
+        )
+
+    except Exception as e:
+        logger.error(f"Error transforming ReyRey record - {record}: {e}")
+        raise
+
+
+def lambda_handler(event: Any, context: Any) -> Any:
+    """Transform raw ReyRey_CRM data to the unified format."""
+    logger.info(f"Event: {event}")
+
+    try:
+        processor = SqsFifoPartialProcessor()
+        result = process_partial_response(
+            event=event,
+            record_handler=record_handler,
+            processor=processor,
+            context=context,
+        )
+        return result
+
+    except Exception as e:
+        logger.error(f"Error processing ReyRey new lead: {e}")
+        raise
