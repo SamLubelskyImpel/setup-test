33927700cf0412ef315ac04b216b997222d6335f
Farwa10 <fiftikhar@impel.io>
2024-02-07 23:11:46 -0500

    backend v1

--- dms-api/app/create_dealer_inbound.py
+++ dms-api/app/create_dealer_inbound.py
@@ -1,5 +1,6 @@
 """Runs the create new dealer API"""
 import logging
+import json
 from datetime import date, datetime, timezone
 from json import dumps
 from os import environ
@@ -14,26 +15,84 @@ from dms_orm.models.service_repair_order import ServiceRepairOrder
 from dms_orm.models.vehicle import Vehicle
 from dms_orm.session_config import DBSession
 from sqlalchemy import func, text
+from sqlalchemy.exc import IntegrityError
 
 logger = logging.getLogger()
 logger.setLevel(environ.get("LOGLEVEL", "INFO").upper())
 
+
+def create_new_dealer(session, impel_dealer_id):
+    existing_dealer = session.query(Dealer).filter_by(impel_dealer_id=impel_dealer_id).first()
+    
+    if existing_dealer:
+        raise IntegrityError(f"Dealer with impel_dealer_id '{impel_dealer_id}' already exists.", '', '')
+    new_dealer = Dealer(impel_dealer_id=impel_dealer_id, db_creation_date=datetime.utcnow())
+    session.add(new_dealer)
+    session.commit()
+    return new_dealer  
+
+
+def create_dealer_integration(session, impel_integration_partner_id, impel_dealer_id, dms_id, is_active=True):
+    integration_partner = session.query(IntegrationPartner).filter_by(
+        impel_integration_partner_id=impel_integration_partner_id
+        ).first()
+    
+    if not integration_partner:
+        raise ValueError(f"Integration partner {impel_integration_partner_id} does not exist.")
+
+    try:
+        dealer = create_new_dealer(session, impel_dealer_id)
+    except IntegrityError:
+        raise ValueError(f"Dealer with impel_dealer_id '{impel_dealer_id}' already exists.")
+   
+    existing_integration = session.query(DealerIntegrationPartner).filter_by(
+        dealer_id=dealer.id, 
+        integration_partner_id=integration_partner.id, 
+        dms_id=dms_id
+    ).first()
+    
+    if existing_integration:
+        raise ValueError(f"Dealer Integration {existing_integration.id} already exists.")
+    
+    new_integration = DealerIntegrationPartner(
+        integration_partner_id=integration_partner.id, 
+        dealer_id=dealer.id, 
+        dms_id=dms_id, 
+        is_active=is_active,
+        db_creation_date=datetime.utcnow()
+    )
+    session.add(new_integration)
+    session.commit()
+    return new_integration
+
+
 def create_dealer_handler(event, context):
-    """Create dealer API endpoint."""
     logger.info(f"Received request to create a new dealer: {event}")
-    
-    # Perform any necessary validation or processing here
-    
-    # Assuming successful creation, return a 200 response
-    return {
-        "statusCode": 200,
-        "body": dumps({"message": "Dealer created successfully."}),
-    }
+    body = json.loads(event['body'])
+
+    impel_integration_partner_id = body.get('impel_integration_partner_id')
+    impel_dealer_id = body.get('impel_dealer_id')
+    dms_id = body.get('dms_id')
+
+    with DBSession() as session:
+        try:
+            dealer_integration = create_dealer_integration(session, impel_integration_partner_id, impel_dealer_id, dms_id)
+            logger.info(f"Dealer integration created: {dealer_integration}")
+        except ValueError as e:
+            logger.error(e)
+            return {"statusCode": 400, "body": dumps({"message": str(e)})}
+        except Exception as e:
+            logger.error(f"Unexpected error: {e}")
+            return {"statusCode": 500, "body": dumps({"message": "Internal server error"})}
+
+    return {"statusCode": 200, "body": dumps({"message": "Dealer created successfully."})}
+
 
 def lambda_handler(event, context):
     """Run repair order API."""
 
     logger.info(f"Event: {event}")
+    print("in lambda handler")
     try:
         return create_dealer_handler(event, context)
     except Exception:
