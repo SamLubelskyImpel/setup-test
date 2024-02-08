5320034d175fa03f37bd1f28e5dcd692e7db7aea
Farwa10 <fiftikhar@impel.io>
2024-02-08 16:14:47 -0500

    update

--- dms-api/app/create_dealer_inbound.py
+++ dms-api/app/create_dealer_inbound.py
@@ -21,18 +21,26 @@ logger = logging.getLogger()
 logger.setLevel(environ.get("LOGLEVEL", "INFO").upper())
 
 
-def create_new_dealer(session, impel_dealer_id):
+def create_new_dealer(session, impel_dealer_id, location_name, state, city, zip_code, full_name):
     existing_dealer = session.query(Dealer).filter_by(impel_dealer_id=impel_dealer_id).first()
     
     if existing_dealer:
         raise IntegrityError(f"Dealer with impel_dealer_id '{impel_dealer_id}' already exists.", '', '')
-    new_dealer = Dealer(impel_dealer_id=impel_dealer_id, db_creation_date=datetime.utcnow())
+    new_dealer = Dealer(
+        impel_dealer_id=impel_dealer_id,
+        db_creation_date=datetime.utcnow(),
+        location_name = location_name,
+        state = state,
+        city = city,
+        zip_code = zip_code,
+        # full_name = full_name
+    )
     session.add(new_dealer)
     session.commit()
     return new_dealer  
 
 
-def create_dealer_integration(session, impel_integration_partner_id, impel_dealer_id, dms_id, is_active=True):
+def create_dealer_integration(session, impel_integration_partner_id, impel_dealer_id, dms_id, dealer_id, is_active=True):
     integration_partner = session.query(IntegrationPartner).filter_by(
         impel_integration_partner_id=impel_integration_partner_id
         ).first()
@@ -40,13 +48,8 @@ def create_dealer_integration(session, impel_integration_partner_id, impel_deale
     if not integration_partner:
         raise ValueError(f"Integration partner {impel_integration_partner_id} does not exist.")
 
-    try:
-        dealer = create_new_dealer(session, impel_dealer_id)
-    except IntegrityError:
-        raise ValueError(f"Dealer with impel_dealer_id '{impel_dealer_id}' already exists.")
-   
     existing_integration = session.query(DealerIntegrationPartner).filter_by(
-        dealer_id=dealer.id, 
+        dealer_id=dealer_id , 
         integration_partner_id=integration_partner.id, 
         dms_id=dms_id
     ).first()
@@ -56,10 +59,10 @@ def create_dealer_integration(session, impel_integration_partner_id, impel_deale
     
     new_integration = DealerIntegrationPartner(
         integration_partner_id=integration_partner.id, 
-        dealer_id=dealer.id, 
+        dealer_id=dealer_id, 
         dms_id=dms_id, 
         is_active=is_active,
-        db_creation_date=datetime.utcnow()
+        db_creation_date=datetime.utcnow(),
     )
     session.add(new_integration)
     session.commit()
@@ -73,10 +76,20 @@ def create_dealer_handler(event, context):
     impel_integration_partner_id = body.get('impel_integration_partner_id')
     impel_dealer_id = body.get('impel_dealer_id')
     dms_id = body.get('dms_id')
+    location_name = body.get('location_name')
+    state = body.get('state')
+    city = body.get('city')
+    zip_code = body.get('zip_code')
+    full_name = body.get('full_name')
 
     with DBSession() as session:
         try:
-            dealer_integration = create_dealer_integration(session, impel_integration_partner_id, impel_dealer_id, dms_id)
+            dealer = create_new_dealer(session, impel_dealer_id, location_name, state, city, zip_code, full_name)
+            dealer_id = dealer.id
+        except IntegrityError:
+            raise ValueError(f"Dealer with impel_dealer_id '{impel_dealer_id}' already exists.")
+        try:
+            dealer_integration = create_dealer_integration(session, impel_integration_partner_id, impel_dealer_id, dms_id, dealer_id)
             logger.info(f"Dealer integration created: {dealer_integration}")
         except ValueError as e:
             logger.error(e)
