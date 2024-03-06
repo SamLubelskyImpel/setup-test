c84ca278a06269cdc0007677c5c2ad16710c754c
Stephen Woods <srwoods@impel.io>
2024-03-06 17:29:09 -0500

    ftp lambdas

--- inventory-integrations/coxau/app/download_ftp_coxau.py
+++ inventory-integrations/coxau/app/download_ftp_coxau.py
@@ -1,12 +1,124 @@
+import boto3
+import json
 import logging
-from os import environ
+import os
+import tempfile
+from typing import Any
+from ftplib import FTP
+from datetime import datetime
+from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSRecord
+from aws_lambda_powertools.utilities.batch import (
+    BatchProcessor,
+    EventType,
+    process_partial_response,
+)
 
-ENVIRONMENT = environ["ENVIRONMENT"]
+INVENTORY_BUCKET = os.environ["INVENTORY_BUCKET"]
+ENVIRONMENT = os.environ["ENVIRONMENT"]
+SECRET_KEY = os.environ["FTP_SECRET_KEY"]
 
 logger = logging.getLogger()
-logger.setLevel(environ.get("LOGLEVEL", "INFO").upper())
+logger.setLevel(os.environ.get("LOGLEVEL", "INFO").upper())
+sm_client = boto3.client("secretsmanager")
+s3_client = boto3.client("s3")
 
 
-def lambda_handler(event, context):
+def get_ftp_secrets(secret_name: Any, secret_key: Any) -> Any:
+    """Get FTP secret from Secrets Manager."""
+    secret = sm_client.get_secret_value(
+        SecretId=f"{'prod' if ENVIRONMENT == 'prod' else 'test'}/{secret_name}"
+    )
+    secret = json.loads(secret["SecretString"])[str(secret_key)]
+    secret_data = json.loads(secret)
+
+    return secret_data["hostname"], secret_data["username"], secret_data["password"]
+
+
+def connect_ftp_server(hostname, username, password):
+    """Connect to FTP server and return the connection."""
+    ftp = FTP(hostname)
+    ftp.login(username, password)
+    return ftp
+
+
+def download_and_process_file(ftp, remote_file_path, local_folder):
+    """Download and process a file from the FTP server."""
+    # Change to the local folder where you want to save the file
+    os.chdir(local_folder)
+
+    # Use RETR command to download the file
+    local_file_name = os.path.basename(remote_file_path)
+    with open(local_file_name, 'wb') as local_file:
+        ftp.retrbinary('RETR ' + remote_file_path, local_file.write)
+
+    logger.info(f"File {local_file_name} downloaded successfully.")
+
+    upload_to_s3(local_file_name, remote_file_path)
+    os.remove(local_file_name)
+
+
+def upload_to_s3(local_filename, filename):
+    """Upload files to S3."""
+    format_string = '%Y/%m/%d/%H'
+    date_key = datetime.utcnow().strftime(format_string)
+
+    s3_key = f"raw/coxau/{date_key}/{filename}"
+    s3_client.upload_file(
+        Filename=local_filename,
+        Bucket=INVENTORY_BUCKET,
+        Key=s3_key
+    )
+    logger.info(f"File {filename} uploaded to S3.")
+
+
+def record_handler(record: SQSRecord) -> Any:
+    """Download files from the FTP server and upload to S3."""
+    logger.info(f"Record: {record}")
+
+    try:
+        body = json.loads(record["body"])
+        folder_name = body["folder"]
+        file_names = body["files"]
+
+        if not file_names:
+            logger.info("No files to download from the FTP server.")
+            return
+
+        # Get FTP secrets
+        hostname, username, password = get_ftp_secrets("inventory-integrations-ftp", SECRET_KEY)
+
+        # Connect to FTP server
+        ftp_conn = connect_ftp_server(hostname, username, password)
+        ftp_conn.cwd(folder_name)
+
+        # Process files one by one
+        for remote_file_path in file_names:
+            # Create a temporary directory to download the file
+            with tempfile.TemporaryDirectory() as temp_dir:
+                # Download and process the file
+                download_and_process_file(ftp_conn, remote_file_path, temp_dir)
+
+        ftp_conn.quit()
+
+    except Exception as e:
+        logger.error(f"Error occurred while downloading files from FTP server: {e}")
+        raise
+
+
+def lambda_handler(event: Any, context: Any) -> Any:
+    """Download files from the FTP server and upload to S3."""
     logger.info(f"Event: {event}")
-    pass
+
+    try:
+        processor = BatchProcessor(event_type=EventType.SQS)
+        result = process_partial_response(
+            event=event,
+            record_handler=record_handler,
+            processor=processor,
+            context=context
+        )
+        return result
+
+    except Exception as e:
+        logger.error(f"Error processing ReyRey new lead: {e}")
+        raise
--- inventory-integrations/coxau/app/invoke_ftp_coxau.py
+++ inventory-integrations/coxau/app/invoke_ftp_coxau.py
@@ -1,21 +1,19 @@
 import logging
-from os import environ, path
-from datetime import datetime
+from os import environ
+from datetime import datetime, timedelta, timezone
 from ftplib import FTP
 import json
 import boto3
 from typing import Any
 
-environ["ENVIRONMENT"] = "test"
-SECRET_KEY = "COXAU_FTP"
-
 ENVIRONMENT = environ["ENVIRONMENT"]
-# DOWNLOAD_QUEUE_URL = environ.get("DOWNLOAD_QUEUE_URL")
-# SECRET_KEY = environ.get("FTP_SECRET_KEY")
+DOWNLOAD_QUEUE_URL = environ["DOWNLOAD_QUEUE_URL"]
+SECRET_KEY = environ["FTP_SECRET_KEY"]
 
 logger = logging.getLogger()
 logger.setLevel(environ.get("LOGLEVEL", "INFO").upper())
 sm_client = boto3.client("secretsmanager")
+sqs_client = boto3.client("sqs")
 
 
 def get_ftp_secrets(secret_name: Any, secret_key: Any) -> Any:
@@ -43,56 +41,60 @@ def list_files(ftp, folder):
     return files
 
 
-def upload_file(ftp, local_file_path, remote_folder):
-    # Change to the remote folder on the FTP server
-    ftp.cwd(remote_folder)
-
-    # Open the local file in binary mode for uploading
-    with open(local_file_path, 'rb') as local_file:
-        # Use STOR command to upload the file
-        ftp.storbinary('STOR ' + path.basename(local_file_path), local_file)
-
-    print(f"File '{path.basename(local_file_path)}' uploaded successfully.")
-
-
 def get_file_modification_time(ftp, file_name):
-    # Use the modified timestamp of the file on the server
+    """Get the modification time of a file on the FTP server."""
     modification_time_str = ftp.sendcmd('MDTM ' + file_name)
-    modification_time = datetime.strptime(modification_time_str[4:], '%Y%m%d%H%M%S.%f')
+    modification_time = datetime.strptime(modification_time_str[4:], '%Y%m%d%H%M%S.%f').replace(tzinfo=timezone.utc)
     return modification_time
 
 
-def lambda_handler(event, context):
-    """List modified files in the FTP server."""
+def send_to_download_queue(message):
+    """Send message to the download queue."""
+    try:
+        response = sqs_client.send_message(
+            QueueUrl=DOWNLOAD_QUEUE_URL,
+            MessageBody=json.dumps(message),
+        )
+        logger.info(f"Message {message} added to the download queue: {response}")
+    except Exception as e:
+        logger.error(f"Error occurred while sending message to the download queue: {e}")
+        raise e
+
+
+def lambda_handler(event: Any, context: Any) -> Any:
+    """Check for modified feed files on the FTP server."""
     logger.info(f"Event: {event}")
 
     try:
+        current_time = datetime.now(timezone.utc)
+        last_modified_time = current_time - timedelta(minutes=150)  # 2.5 hours ago
+        logger.info(f"Checking for files modified since {last_modified_time.isoformat()}")
+
         hostname, username, password = get_ftp_secrets("inventory-integrations-ftp", SECRET_KEY)
-        # hostname = "swipetospin.exavault.com"
-        # username = "stssftp_coxau_reader"
-        # password = "%XP$sPDvXTUd36#*5s6mNW!cxCgT3YBCawfi6zFy@LxhQG7nizx7z9r4ig7acX@6"
         ftp_conn = connect_ftp_server(hostname, username, password)
 
         # List files in the folder
         folder_name = "coxau/"
         files = list_files(ftp_conn, folder_name)
-        print("Files in the folder:", files)
+        logger.info(f"Files in the folder: {files}")
 
-        # def file_filter(file):
-            # modification_time = get_file_modification_time(ftp_conn, file)
-            # print(f"File '{file}' was last modified on {modification_time}")
-            # return file.endswith(".csv")
-
-        for file in files:
+        def file_filter(file):
             modification_time = get_file_modification_time(ftp_conn, file)
-            print(f"File '{file}' was last modified on {modification_time}")
+            logger.info(f"File '{file}' was last modified on {modification_time}")
+            return modification_time > last_modified_time
 
+        updated_files = list(filter(file_filter, files))
+        logger.info(f"Updated files: {updated_files}")
         ftp_conn.quit()
 
+        message = {
+            "folder": folder_name,
+            "files": updated_files,
+            "search_time": current_time.isoformat()
+        }
+        logger.info(f"Message to be sent to the download queue: {message}")
+        send_to_download_queue(message)
+
     except Exception as e:
-        logger.error(f"Failed to get FTP secrets: {e}")
+        logger.error(f"Error occurred while checking FTP for modified files: {e}")
         raise
-
-
-if __name__ == "__main__":
-    lambda_handler({}, {})
