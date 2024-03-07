87d323a12e9d6140af7f7a67e72558b0e34991d0
Konstantine Bakradze <konstantine4w@gmail.com>
2024-03-07 16:33:12 +0400

    correct deploy script

--- crm-integrations/adf-assembler/deploy.sh
+++ crm-integrations/adf-assembler/deploy.sh
@@ -39,7 +39,6 @@ fi
 user=$(aws iam get-user --output json | jq -r .User.UserName)
 commit_id=$(git log -1 --format=%H)
 
-python3 ./swagger/oas_interpolator.py
 sam build --parallel
 
 if [[ $config_env == "prod" ]]; then
@@ -59,13 +58,13 @@ elif [[ $config_env == "test" ]]; then
     --tags "Commit=\"$commit_id\" Environment=\"test\" UserLastModified=\"$user\"" \
     --region "$region" \
     --s3-bucket "spincar-deploy-$region" \
-    --parameter-overrides "Environment=\"test\" DomainSuffix=\"\""
+    --parameter-overrides "Environment=\"test\""
 else
   env="$user-$(git rev-parse --abbrev-ref HEAD)"
   sam deploy \
     --tags "Commit=\"$commit_id\" Environment=\"$env\" UserLastModified=\"$user\"" \
-    --stack-name "crm-partner-api-$env" \
+    --stack-name "adf-assembler-$env" \
     --region "$region" \
     --s3-bucket "spincar-deploy-$region" \
-    --parameter-overrides "Environment=\"$env\" DomainSuffix=\"-$env\""
+    --parameter-overrides "Environment=\"$env\""
 fi
