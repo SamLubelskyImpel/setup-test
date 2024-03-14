acb5dfa22ffaeb13a14e3cfca43ac5f2eac242f9
Konstantine Bakradze <konstantine4w@gmail.com>
2024-03-14 23:16:26 +0400

    change environment

--- crm-integrations/adf-assembler/app/create-adf/adf_assemble.py
+++ crm-integrations/adf-assembler/app/create-adf/adf_assemble.py
@@ -48,21 +48,22 @@ def assemble_adf(body, lead_data):
 def lambda_handler(event: Any, context: Any) -> Any:
     try:
         logger.info(f"Event: {event}")
-        body = loads(event["body"])
-        logger.info(f"Body: {body}")
+        body = loads(event["body"]) if event.get("body") else event
 
         api_wrapper = ApiWrapper()
         lead_data = api_wrapper.get_lead(body.get("lead_id"))
-        logger.log(f"[adf_assembler] data from lead_id: \n{dumps(lead_data)}")
+        logger.info(f"[adf_assembler] data from lead_id: \n{lead_data}")
 
         formatted_adf = assemble_adf(body, lead_data)
-        logger.log(f"[adf_assembler] adf file: \n{formatted_adf}")
+        logger.info(f"[adf_assembler] adf file: \n{formatted_adf}")
 
         partner_id = lead_data.get("integration_partner_name")
         current_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-        logger.log(f"Partner ID: {partner_id} \nCurrent date: {current_time}")
+        logger.info(f"Partner ID: {partner_id}")
 
         s3_key = f"email-service-store-{ENVIRONMENT}/chatai/{partner_id}_{body.get('lead_id')}_{current_time}.json"
+
+        logger.info(f"take object from: configurations/{ENVIRONMENT}_{partner_id}.json \n Bucket: {BUCKET}")
 
         s3_object = loads(
             s3_client.get_object(
@@ -72,20 +73,27 @@ def lambda_handler(event: Any, context: Any) -> Any:
         recipients = s3_object.get("recipients")
         integration_type = s3_object.get("adf_integration_type")
 
+        logger.info(f"recipients: {recipients} \n integration_type: {integration_type}")
+
         if integration_type == "EMAIL":
+            s3_body = dumps(
+                {
+                    "recipients": recipients,
+                    "subject": "Lead ADF From Impel",
+                    "body": formatted_adf,
+                    "from_address": "crm.adf@impel.ai",
+                    "reply_to": [],
+                }
+            )
             s3_client.put_object(
-                Body=dumps(
-                    {
-                        "recipients": recipients,
-                        "subject": "Lead ADF From Impel",
-                        "body": formatted_adf,
-                        "from_address": "crm.adf@impel.ai",
-                        "reply_to": [],
-                    }
-                ),
+                Body=s3_body,
                 Bucket=BUCKET,
                 Key=s3_key,
             )
+            return {
+                "statusCode": 200,
+                "body": dumps({"message": "Adf file was successfully created."})
+            }
     except Exception as e:
         logger.exception(f"Error creating lead: {e}.")
         return {
