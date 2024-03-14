74b123c560d54882b2ce601a00f7e721e26fec19
Konstantine Bakradze <konstantine4w@gmail.com>
2024-03-14 19:16:30 +0400

    change authorization method

--- crm-integrations/adf-assembler/app/authorizer.py
+++ crm-integrations/adf-assembler/app/authorizer.py
@@ -1,11 +1,10 @@
-"""Authorize requests to the ADF assembler."""
+"""Authorize requests to the adf assembler gateway."""
 import json
 import logging
 import os
 from typing import Any, Dict
 
 import boto3
-from base64 import b64decode
 from botocore.exceptions import ClientError
 
 logger = logging.getLogger()
@@ -14,16 +13,42 @@ logger.setLevel(os.environ.get("LOGLEVEL", "INFO").upper())
 is_prod = os.environ.get("ENVIRONMENT", "test") == "prod"
 
 
-def decode_basic_auth(basic_auth_str: str):
-    """Decode a given basic auth."""
-    try:
-        input_auth_encoded = basic_auth_str.split("Basic ")[1]
-        input_auth_str = b64decode(input_auth_encoded).decode("utf-8")
-        client_id, input_auth = input_auth_str.split(":")
-        return client_id, input_auth
-    except (ValueError, IndexError):
-        logger.exception(f"Malformed input basic auth {basic_auth_str}")
-        return None, None
+def is_endpoint_allowed(method_arn: str, allowed_endpoints: Any) -> bool:
+    """Check if the requested endpoint and method are in the set of allowed endpoints for the partner."""
+    if allowed_endpoints == ["*"]:
+        return True
+
+    arn_parts = method_arn.split(':')[-1].split('/')
+    http_method = arn_parts[2]
+    request_path = '/' + '/'.join(arn_parts[3:])
+
+    for allowed_endpoint in allowed_endpoints:
+        allowed_method, allowed_path = allowed_endpoint.split('/', 1)
+        if http_method != allowed_method:
+            continue
+
+        if match_path(request_path, allowed_path):
+            return True
+    return False
+
+
+def match_path(request_path: str, allowed_path: str) -> bool:
+    """Match the request path against the allowed path."""
+    request_segments = request_path.strip('/').split('/')
+    allowed_segments = allowed_path.strip('/').split('/')
+
+    if len(request_segments) != len(allowed_segments):
+        return False
+
+    for request_segment, allowed_segment in zip(request_segments, allowed_segments):
+        # If the allowed segment is a placeholder (e.g., "{lead_id}"), we skip the comparison
+        # Otherwise, we compare the segments directly
+        if allowed_segment.startswith('{') and allowed_segment.endswith('}'):
+            continue
+        if request_segment != allowed_segment:
+            return False
+
+    return True
 
 
 def _lambda_handler(event: Any, context: Any) -> Any:
@@ -32,9 +57,9 @@ def _lambda_handler(event: Any, context: Any) -> Any:
 
     method_arn = event["methodArn"]
 
-    auth = event['authorizationToken']
-
-    partner_id, api_key = decode_basic_auth(auth)
+    headers = {k.lower(): v for k, v in event['headers'].items()}
+    partner_id = headers.get('partner_id')
+    api_key = headers.get('x_api_key')    
 
     SM_CLIENT = boto3.client("secretsmanager")
 
@@ -62,11 +87,15 @@ def _lambda_handler(event: Any, context: Any) -> Any:
         raise Exception("Unauthorized")
 
     authorized = api_key == secret_data["api_key"]
-    if authorized:
+    allowed_endpoints = secret_data.get("endpoints", [])
+    endpoint_allowed = is_endpoint_allowed(method_arn, allowed_endpoints)
+
+    if authorized and endpoint_allowed:
         policy["Statement"][0]["Effect"] = "Allow"
         return {"policyDocument": policy, "principalId": partner_id}
-
-    raise Exception("Unauthorized")
+    else:
+        logger.info(f"Access denied for partner_id {partner_id} to endpoint {method_arn}")
+        raise Exception("Unauthorized")
 
 
 def lambda_handler(event: Any, context: Any) -> Any:
