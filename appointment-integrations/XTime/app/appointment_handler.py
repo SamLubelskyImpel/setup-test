43f156a0ab7eb5eb6dcb32847de6196f4a9ac633
Konstantine Bakradze <konstantine4w@gmail.com>
2024-05-02 15:09:46 +0400

    make small changes for pytests

--- appointment-integrations/XTime/app/appointment_handler.py
+++ appointment-integrations/XTime/app/appointment_handler.py
@@ -8,6 +8,7 @@ from models import GetAppointments, CreateAppointment, AppointmentSlots
 logger = logging.getLogger()
 logger.setLevel(environ.get("LOGLEVEL", "INFO").upper())
 
+
 def get_appt_time_slots(event: Any, context: Any) -> Any:
     try:
         logger.info(f"Event: {event}")
@@ -16,37 +17,49 @@ def get_appt_time_slots(event: Any, context: Any) -> Any:
             appointment_slots = AppointmentSlots(**body)
         else:
             appointment_slots = AppointmentSlots(**event)
-        
+
         api_wrapper = XTimeApiWrapper()
 
-        appt_time_slots = api_wrapper.retrieve_appt_time_slots(appointment_slots=appointment_slots)
-        
+        appt_time_slots = api_wrapper.retrieve_appt_time_slots(
+            appointment_slots=appointment_slots
+        )
+
+        logger.info(appt_time_slots["availableAppointments"])
+
         if appt_time_slots["success"]:
             return {
                 "statusCode": 200,
-                "body": {
+                "body": dumps({
                     "available_timeslots": [
                         {
                             "timeslot": time_slot["appointmentDateTimeLocal"],
-                            "duration": time_slot["durationMinutes"]
-                        } for time_slot in appt_time_slots["availableAppointments"]
+                            "duration": time_slot["durationMinutes"],
+                        }
+                        for time_slot in appt_time_slots["availableAppointments"]
                     ]
-                }
+                })
             }
 
         return {
-            "statusCode": 401,
-            "body": dumps({"message": f"Response from XTime: {appt_time_slots["message"]}."}),
+            "statusCode": 500,
+            "body": dumps(
+                {
+                    "error": {
+                        "code": appt_time_slots["code"],
+                        "message": appt_time_slots["message"],
+                    }
+                }
+            ),
         }
-
+    
     except Exception as e:
         logger.exception(f"Error creating lead: {e}.")
         return {
             "statusCode": 500,
             "body": dumps({"error": "An error occurred while processing the request."}),
         }
-    
-    
+
+
 def create_appointment(event: Any, context: Any) -> Any:
     try:
         logger.info(f"Event: {event}")
@@ -55,22 +68,22 @@ def create_appointment(event: Any, context: Any) -> Any:
             create_appointment_data = CreateAppointment(**body)
         else:
             create_appointment_data = CreateAppointment(**event)
-        
+
         api_wrapper = XTimeApiWrapper()
-        
+
         create_appointment = api_wrapper.create_appointments(create_appointment_data)
-        
+
         if create_appointment["success"]:
             return {
                 "statusCode": 201,
-                "body": {
-                    "appointment_id": create_appointment["appointmentId"]
-                }
+                "body": {"appointment_id": create_appointment["appointmentId"]},
             }
-        
+
         return {
             "statusCode": 401,
-            "body": dumps({"message": f"Response from XTime: {create_appointment["message"]}."}),
+            "body": dumps(
+                {"message": f"Response from XTime: {create_appointment['message']}."}
+            ),
         }
 
     except Exception as e:
@@ -79,7 +92,7 @@ def create_appointment(event: Any, context: Any) -> Any:
             "statusCode": 500,
             "body": dumps({"error": "An error occurred while processing the request."}),
         }
-    
+
 
 def get_appointments(event: Any, context: Any) -> Any:
     try:
@@ -89,41 +102,54 @@ def get_appointments(event: Any, context: Any) -> Any:
             get_appointments_data = GetAppointments(**body)
         else:
             get_appointments_data = GetAppointments(**event)
-        
+
         api_wrapper = XTimeApiWrapper()
-        
+
         appointments_data = api_wrapper.retrieve_appointments(get_appointments_data)
-        
+
         if appointments_data["success"]:
             return {
                 "statusCode": 200,
-                "body": {
+                "body": dumps({
                     "appointments": [
                         {
-                            "appointment_id": appt["appointmentId"],
-                            "vin": appt["vin"],
-                            "timeslot": appt["appointmentDateTimeLocal"],
-                            "timeslot_duration": appt["appointmentDateTimeLocal"],
-                            "comment": appt["comment"],
-                            "first_name": appt["firstName"],
-                            "last_name": appt["lastName"],
-                            "email_address": appt["emailAddress"],
-                            "phone_number": appt["phoneNumber"],
-                            "op_code": appt["services"][0]["opcode"]
-                        } for appt in appointments_data["appointments"]
+                            "appointment_id": appt.get("appointmentId"),
+                            "vin": appt.get("vin"),
+                            "timeslot": appt.get("appointmentDateTimeLocal"),
+                            "timeslot_duration": 15,  # It's default for XTime
+                            "comment": appt.get("comment"),
+                            "first_name": appt.get("firstName"),
+                            "last_name": appt.get("lastName"),
+                            "email_address": appt.get("emailAddress"),
+                            "phone_number": appt.get("phoneNumber"),
+                            "services": [
+                                {
+                                    "op_code": service.get("opcode"),
+                                    "service_name": service.get("serviceName"),
+                                }
+                                for service in appt["services"]
+                            ],
+                        }
+                        for appt in appointments_data["appointments"]
                     ]
-                }
+                },)
             }
-        
-        return {
-            "statusCode": 401,
-            "body": dumps({"message": f"Response from XTime: {appointments_data["message"]}."}),
-        }
 
+        return {
+            "statusCode": 500,
+            "body": dumps(
+                {
+                    "error": {
+                        "code": appointments_data["code"],
+                        "message": appointments_data["message"],
+                    }
+                }
+            ),
+        }
 
     except Exception as e:
         logger.exception(f"Error creating lead: {e}.")
         return {
             "statusCode": 500,
-            "body": dumps({"error": "An error occurred while processing the request."}),
+            "body": dumps({"error": f"An error occurred while processing the request.\n{e}"}),
         }
--- appointment-integrations/XTime/app/xtime_api_wrapper.py
+++ appointment-integrations/XTime/app/xtime_api_wrapper.py
@@ -30,33 +30,18 @@ class XTimeApiWrapper:
 
 
     def __get_secrets(self):
-        # secret = secret_client.get_secret_value(
-        #     SecretId=f"{'prod' if ENVIRONMENT == 'prod' else 'test'}/appt-integration-partners"
-        # )
-        # secret = loads(secret["SecretString"])["XTime"]
-        # secret_data = loads(secret)
-        test_dict = {
-            "authorization_token_config": {
-                "url":  "https://auth.coxautoinc.com/token",
-                "username": "6d95cc71-8de0-4574-805a-c5f3594fdf4d",
-                "password": "vttqvYiMk783ZB3mEE8S1Ffuxd229sCF3sfGhr4xWYvI9paCohIxr4ErD"
-            },
-            "x_api_key": "1Dn57QXZxo1Jt8f97lrJ337dLhwTr1Uf6tHytBQb",
-            "api_url": "https://sandbox.api.coxautoinc.com/xtime", 
-            "dealer_code": "xts9010"
-        }
-        return (
-            test_dict["authorization_token_config"],
-            test_dict["x_api_key"],
-            test_dict["api_url"],
-            test_dict["dealer_code"]
+        secret = secret_client.get_secret_value(
+            SecretId=f"{'prod' if ENVIRONMENT == 'prod' else 'test'}/appt-integration-partners"
         )
-        # return (
-        #     secret_data["API_URL"],
-        #     secret_data["X_API_KEY"],
-        #     secret_data["DEVELOPER_KEY"]
-        # )
-
+        secret = loads(secret["SecretString"])["XTime"]
+        secret_data = loads(secret)
+        logger.info(secret_data)
+        return (
+            secret_data["authorization_token_config"],
+            secret_data["x_api_key"],
+            secret_data["api_url"],
+            secret_data["dealer_code"]
+        )
 
     def __get_token(self):
         url, username, password = self.__authorization_data.values()
@@ -85,7 +70,7 @@ class XTimeApiWrapper:
         }
         try:
             response = requests.request(method=method, url=url, json=payload, headers=headers, params=params)
-            logger.info(f"Response from CRM: {response}")
+            logger.info(f"Response from XTime: {response.json()}")
             
             response.raise_for_status()
             return response.json()
@@ -96,18 +81,18 @@ class XTimeApiWrapper:
 
     def __convert_utc_to_timezone(self, input_ts: str, dealer_timezone: str) -> str:
         """Convert UTC timestamp to dealer's local time."""
-        utc_datetime = datetime.strptime(input_ts, '%Y-%m-%dT%H:%M:%SZ')
+        utc_datetime = datetime.strptime(input_ts, '%Y-%m-%dT%H:%M:%S')
         utc_datetime = pytz.utc.localize(utc_datetime)
 
         if not dealer_timezone:
             logger.warning("Dealer timezone not found")
-            return utc_datetime.strftime('%Y-%m-%dT%H:%M:%S')
+            return utc_datetime.strftime('%Y-%m-%d')
 
         # Get the dealer timezone object, convert UTC datetime to dealer timezone
         dealer_tz = pytz.timezone(dealer_timezone)
         dealer_datetime = utc_datetime.astimezone(dealer_tz)
 
-        return dealer_datetime.strftime('%Y-%m-%dT%H:%M:%S')
+        return dealer_datetime.strftime('%Y-%m-%d')
     
 
     def create_appointments(self, create_appt_data: CreateAppointment):
@@ -156,17 +141,14 @@ class XTimeApiWrapper:
 
     def retrieve_appointments(self, appointments: GetAppointments):
         """Retrieve appointments on XTime."""
-        url = "{}/appointments".format(self.__api_url)
+        url = "{}/service/appointments".format(self.__api_url)
 
         params = {
             "dealerCode": self.__dealer_code,
             "vin": appointments.vin
         }
 
-        response_json = self.__call_api(url, method="GET", params=params)
-
-        logger.info(f"Response from CRM: {response_json}")
-        return response_json["appointments"]
+        return self.__call_api(url, method="GET", params=params)
 
 
     def retrieve_appt_time_slots(self, appointment_slots: AppointmentSlots):
@@ -175,14 +157,11 @@ class XTimeApiWrapper:
 
         params = {
             "dealerCode": appointment_slots.integration_dealer_id,
+            "start": self.__convert_utc_to_timezone(appointment_slots.start_time, appointment_slots.dealer_timezone),
+            "end": self.__convert_utc_to_timezone(appointment_slots.end_time, appointment_slots.dealer_timezone),
+            "opcode": appointment_slots.op_code,
+            "vin": appointment_slots.vin
         }
-
-        parameters = ["year", "make", "model", "op_code", "start_time", "end_time"]
-
-        for param in parameters:
-            value = getattr(appointment_slots, param, None)
-            if value:
-                params[param] = value
 
         response_json = self.__call_api(url, method="GET", params=params)
 
