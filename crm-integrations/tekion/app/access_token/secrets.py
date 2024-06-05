ffb20decb6423718ff0ad55a8e7006cdd26ed04f
Hugo Seabra <hugo.nearsure@impel.ai>
2024-06-05 18:55:21 -0300

    fix: removing auth secrets id from template

--- crm-integrations/tekion/app/access_token/secrets.py
+++ crm-integrations/tekion/app/access_token/secrets.py
@@ -1,12 +1,13 @@
-from os import environ
-from aws_lambda_powertools import Logger
 import boto3
+from aws_lambda_powertools import Logger
+from os import environ
 
 from .schemas import TekionCredentials
 
 ENVIRONMENT = environ.get("ENVIRONMENT", "test")
 SECRETS_MANAGER_ID = environ.get(
-    "SECRETS_MANAGER_ID", f"{"prod" if ENVIRONMENT == "prod" else "test" }/crm-tekion"
+    "TEKION_AUTH_SECRETS_ID",
+    f"{"prod" if ENVIRONMENT == "prod" else "test"}/crm-tekion"
 )
 
 logger = Logger()
@@ -16,10 +17,10 @@ def get_credentials_from_secrets() -> TekionCredentials:
     logger.debug(
         "Fetching credentials from Secrets Manager",
         extra={
-            "secrets_manager_id": SECRETS_MANAGER_ID
+            "secrets_manager_id": TEKION_AUTH_SECRETS_ID
         }
     )
     client = boto3.client("secretsmanager")
-    response = client.get_secret_value(SecretId=SECRETS_MANAGER_ID)
+    response = client.get_secret_value(SecretId=TEKION_AUTH_SECRETS_ID)
     content = response["SecretString"]
     return TekionCredentials.model_validate_json(json_data=content)
