371bf249f080de624e38692b91c1d1e817be1327
Hugo Seabra <hugo.nearsure@impel.ai>
2024-06-07 15:59:13 -0300

    feat: setting up module only for env vars

---
+++ crm-integrations/tekion/app/access_token/envs.py
@@ -0,0 +1,23 @@
+from os import environ
+
+
+# Environment
+LOGLEVEL = environ.get("LOGLEVEL", "INFO")
+ENV = "prod" if environ.get("ENVIRONMENT", "test") == "prod" else "test"
+REGION = environ.get("REGION", "us-east-1")
+
+# Partner
+PARTNER_KEY = environ.get("TEKION_PARTNER_KEY", "TEKION")
+
+# S3
+INTEGRATIONS_BUCKET = environ.get("INTEGRATIONS_BUCKET", f"crm-integrations-{ENV}")
+TOKEN_FILE = environ.get("TOKEN_FILE", f"{PARTNER_KEY.lower()}/token.json")
+
+# Secrets Manager
+CRM_INTEGRATION_SECRETS_ID = environ.get(
+    "CRM_INTEGRATION_SECRETS_ID",
+    f"{"prod" if ENV == "prod" else "test"}/crm-integrations-partner"
+)
+
+# Tekion API
+CRM_TEKION_AUTH_ENDPOINT = environ.get("CRM_TEKION_AUTH_ENDPOINT", "/oauth/token")
