1e9840b76723c1ba0bf8cd38ad83fde42e67c7bc
louis <louis@impel.ai>
2024-08-22 14:10:31 -0400

    get crm dealer id from event

--- crm-integrations/pbs/app/get_dealer_salespersons_handler.py
+++ crm-integrations/pbs/app/get_dealer_salespersons_handler.py
@@ -10,12 +10,12 @@ def lambda_handler(event, context):
     logger.info("Lambda function invoked with event: %s", event)
 
     api = APIWrapper()
-
+    crm_dealer_id = employee_id = event.get("crm_dealer_id", "00000000000000000000000000000000")
     employee_id = event.get("employee_id", "00000000000000000000000000000000")  # Default EmployeeId for testing
     logger.info("Fetching employee data for EmployeeId: %s", employee_id)
 
     try:
-        result = api.call_employee_get(employee_id)
+        result = api.call_employee_get(employee_id, crm_dealer_id)
         logger.info("Successfully received response from API: %s", result)
     except Exception as e:
         logger.error("Failed to retrieve employee data: %s", e)
