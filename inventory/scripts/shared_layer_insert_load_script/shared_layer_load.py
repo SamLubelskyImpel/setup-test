938356dac0ce48aefd850929d0a3f0195f0e4f13
sachdeva-impel <ssachdeva@impel.ai>
2024-12-11 10:49:53 -0500

    Modified the shared_layer_load

--- inventory/scripts/shared_layer_insert_load_script/shared_layer_load.py
+++ inventory/scripts/shared_layer_insert_load_script/shared_layer_load.py
@@ -45,7 +45,7 @@ if not logger.handlers:
 def extract_vehicle_data(json_data):
     # Attempt to get the year and convert it to an integer if possible
     #  There was an entry whose year was an empty string, so we need to handle this case
-    year_value = json_data.get("inv_vehicle|year")
+    year_value = json_data.get('inv_vehicle|year')
     try:
         # Only attempt the conversion if year_value is not an empty string
         year = int(year_value) if year_value.strip() else None
@@ -53,82 +53,84 @@ def extract_vehicle_data(json_data):
         # Set to None if conversion fails or if year_value is None
         year = None
 
-    new_or_used_raw = json_data.get("inv_vehicle|new_or_used", "").strip().lower()
-    if new_or_used_raw == "new":
-        new_or_used = "N"
-    elif new_or_used_raw == "used":
-        new_or_used = "U"
+    new_or_used_raw = json_data.get('inv_vehicle|new_or_used', '').strip().lower()
+    if new_or_used_raw == 'new':
+        new_or_used = 'N'
+    elif new_or_used_raw == 'used':
+        new_or_used = 'U'
     else:
-        new_or_used = ""
+        new_or_used = ''
 
     vehicle_data = {
-        "vin": json_data.get("inv_vehicle|vin", ""),
-        "oem_name": json_data.get("inv_vehicle|oem_name", ""),
-        "type": json_data.get("inv_vehicle|type", ""),
-        "mileage": json_data.get("inv_vehicle|mileage", ""),
-        "make": json_data.get("inv_vehicle|make", ""),
-        "model": json_data.get("inv_vehicle|model", ""),
-        "year": year,
-        "stock_num": json_data.get("inv_vehicle|stock_num", ""),
-        "new_or_used": new_or_used,
+        'vin': json_data.get('inv_vehicle|vin', ''),
+        'oem_name': json_data.get('inv_vehicle|oem_name', ''),
+        'type': json_data.get('inv_vehicle|type', ''),
+        'mileage': json_data.get('inv_vehicle|mileage', ''),
+        'make': json_data.get('inv_vehicle|make', ''),
+        'model': json_data.get('inv_vehicle|model', ''),
+        'year': year,
+        'stock_num': json_data.get('inv_vehicle|stock_num', ''),
+        'new_or_used': new_or_used,
     }
     return vehicle_data
 
 
 def extract_inventory_data(json_data):
-    def extract_field(field, ftype="str"):
+    def extract_field(field, ftype='str'):
         value = json_data.get(field)
         if not value:
             return None
 
         # Remove spaces from the value for inv_inventory|cylinders
-        if field == "inv_inventory|cylinders":
-            value = value.strip().replace(" ", "")
+        if field == 'inv_inventory|cylinders':
+            value = value.strip().replace(' ', '')
 
-        if ftype == "int":
+        if ftype == 'int':
             return int(value.strip())
-        elif ftype == "float":
+        elif ftype == 'float':
             return float(value.strip())
-        elif ftype == "str":
+        elif ftype == 'str':
             return value.strip()
+        elif ftype == 'list':
+            return value
         else:
             logger.error(f"Invalid field: {ftype}")
             raise ValueError(f"Invalid field type: {ftype}")
 
     inventory_data = {
-        "list_price": extract_field("inv_inventory|list_price", ftype="float"),
-        "special_price": extract_field("inv_inventory|special_price", ftype="float"),
-        "fuel_type": extract_field("inv_inventory|fuel_type"),
-        "exterior_color": extract_field("inv_inventory|exterior_color"),
-        "interior_color": extract_field("inv_inventory|interior_color"),
-        "doors": extract_field("inv_inventory|doors", ftype="int"),
-        "seats": extract_field("inv_inventory|seats", ftype="int"),
-        "transmission": extract_field("inv_inventory|transmission"),
-        "photo_url": extract_field("inv_inventory|photo_url"),
-        "drive_train": extract_field("inv_inventory|drive_train"),
-        "cylinders": extract_field("inv_inventory|cylinders", ftype="int"),
-        "body_style": extract_field("inv_inventory|body_style"),
-        "series": extract_field("inv_inventory|series"),
-        "vin": extract_field("inv_inventory|vin"),
-        "interior_material": extract_field("inv_inventory|interior_material"),
-        "trim": extract_field("inv_inventory|trim"),
-        "factory_certified": json_data.get("inv_inventory|factory_certified", False),
-        "region": extract_field("inv_inventory|region"),
-        "on_lot": json_data.get("inv_inventory|on_lot", True),
-        "metadata": extract_field("inv_inventory|metadata"),
-        "received_datetime": extract_field("inv_inventory|received_datetime"),
-        "vdp": extract_field("inv_inventory|vdp"),
-        "comments": extract_field("inv_inventory|comments"),
+        'list_price': extract_field('inv_inventory|list_price', ftype='float'),
+        'special_price': extract_field('inv_inventory|special_price', ftype='float'),
+        'fuel_type': extract_field('inv_inventory|fuel_type'),
+        'exterior_color': extract_field('inv_inventory|exterior_color'),
+        'interior_color': extract_field('inv_inventory|interior_color'),
+        'doors': extract_field('inv_inventory|doors', ftype='int'),
+        'seats': extract_field('inv_inventory|seats', ftype='int'),
+        'transmission': extract_field('inv_inventory|transmission'),
+        'photo_url': extract_field('inv_inventory|photo_url'),
+        'drive_train': extract_field('inv_inventory|drive_train'),
+        'cylinders': extract_field('inv_inventory|cylinders', ftype='int'),
+        'body_style': extract_field('inv_inventory|body_style'),
+        'series': extract_field('inv_inventory|series'),
+        'vin': extract_field('inv_inventory|vin'),
+        'interior_material': extract_field('inv_inventory|interior_material'),
+        'trim': extract_field('inv_inventory|trim'),
+        'factory_certified': json_data.get('inv_inventory|factory_certified', False),
+        'region': extract_field('inv_inventory|region'),
+        'on_lot': json_data.get('inv_inventory|on_lot', True),
+        'metadata': extract_field('inv_inventory|metadata'),
+        'received_datetime': extract_field('inv_inventory|received_datetime'),
+        'vdp': extract_field('inv_inventory|vdp'),
+        'comments': extract_field('inv_inventory|comments'),
+        'options': extract_field('inv_inventory|options', ftype='list'),
+        'priority_options': extract_field('inv_inventory|priority_options', ftype='list'),
+        "vehicle_data": {  # Fields used to match vehicle data
+            'vin': json_data.get('inv_vehicle|vin', ''),
+            'model': json_data.get('inv_vehicle|model', ''),
+            'stock_num': json_data.get('inv_vehicle|stock_num', ''),
+            'mileage': json_data.get('inv_vehicle|mileage', '')
+        }
     }
     return inventory_data
-
-
-def extract_option_data(option_json):
-    option_data = {
-        "option_description": option_json.get("inv_option|option_description", ""),
-        "is_priority": option_json.get("inv_option|is_priority", False),
-    }
-    return option_data
 
 
 def process_and_upload_data(bucket, key, rds_instance: RDSInstance):
@@ -158,83 +160,45 @@ def process_and_upload_data(bucket, key, rds_instance: RDSInstance):
             )
             return
 
-        processed_inventory_ids = []
-        inserted_inventory_map: dict[int, int] = {}
+        # Accumulate records for batch processing
+        vehicle_data_list = []
+        inventory_data_list = []
 
+        logger.info("Starting data processing.")
         for json_data in json_data_list:
-            # Insert vehicle data
+            # Extract vehicle data
             vehicle_data = extract_vehicle_data(json_data)
-            vehicle_data["dealer_integration_partner_id"] = (
-                dealer_integration_partner_id
-            )
-            vehicle_id = rds_instance.insert_vehicle(vehicle_data)
+            vehicle_data_list.append(vehicle_data)
 
-            if vehicle_id is None:
-                logger.warning(
-                    "Skipping record with stock number %s as it already exists.",
-                    vehicle_data['stock_num']
-                )
-                continue
-            else:
-                logger.info(
-                    "Inserted vehicle record with stock number %s and vehicle ID %d",
-                    vehicle_data["stock_num"],
-                    vehicle_id,
-                )
-
-            # Insert inventory data
+            # Extract inventory data
             inventory_data = extract_inventory_data(json_data)
-            inventory_data["vehicle_id"] = vehicle_id
-            inventory_data["dealer_integration_partner_id"] = (
-                dealer_integration_partner_id
-            )
+            inventory_data_list.append(inventory_data)
 
-            # Set on_lot to False for all data inserted data
-            inventory_data["on_lot"] = False
-            inventory_id = rds_instance.insert_inventory_item(inventory_data)
-            processed_inventory_ids.append(inventory_id)
+        # Perform batch upsert for vehicles
+        vehicle_id_map = rds_instance.batch_upsert_vehicles(vehicle_data_list, dealer_integration_partner_id)
 
-            # Store the inventory_id and vehicle_id in the map
-            inserted_inventory_map[inventory_id] = vehicle_id
+        logger.info(f"Processed vehicles {len(vehicle_id_map)}")
 
-            # Insert options data
-            if json_data.get("inv_options|inv_options"):
-                option_data_list = []
-                for option_json in json_data["inv_options|inv_options"]:
-                    option_data = extract_option_data(option_json)
-                    option_data_list.append(option_data)
-
-                # Perform bulk insert
-                option_ids = rds_instance.insert_options(option_data_list)
-
-                # Link the inserted options to the inventory
-                rds_instance.link_option_to_inventory(inventory_id, option_ids)
-            else:
-                identifier = json_data.get(
-                    json_data.get("inv_vehicle|stock_num", "Unknown Identifier")
+        # Use `vehicle_id_map` to set `vehicle_id` in inventory records before batch processing inventory
+        for inventory_data in inventory_data_list:
+            # Assuming VIN or other key is used to look up the vehicle ID from vehicle_id_map
+            vehicle_key = (
+                    inventory_data['vehicle_data'].get('vin'),
+                    dealer_integration_partner_id,
+                    inventory_data['vehicle_data'].get('model'),
+                    inventory_data['vehicle_data'].get('stock_num'),
+                    inventory_data['vehicle_data'].get('mileage')
                 )
+            inventory_data['vehicle_id'] = vehicle_id_map.get(vehicle_key)
+            if not inventory_data['vehicle_id']:
+                logger.warning(f"Vehicle ID not found for inventory record with key {vehicle_key}")
+                raise
 
-                logger.warning(
-                    f"Skipping options for record with identifier {identifier} as they do not exist."
-                )
+        # Perform batch insert or update for inventory items
+        processed_inventory_ids = rds_instance.batch_insert_inventory(inventory_data_list, dealer_integration_partner_id)
 
-        logger.info(
-            "Data processing completed. Total records processed: %d",
-            len(json_data_list),
-        )
-
-        # Save the inventory_options_map
-        file_name = f"{decoded_key.split('.')[0]}_inventory_options_map.json"
-        file_path = os.path.join(folder_path, file_name)
-
-        # Ensure the directory exists
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-
-        with open(file_path, "w") as file:
-            json.dump(inserted_inventory_map, file)
-
-        logger.info(f"Inventory and options data saved to {file_path}")
-        logger.info(f"Data processing and upload completed for {decoded_key}")
+        logger.info("Data processing completed. Total records processed: %d",
+                    len(json_data_list))
 
     except Exception:
         logger.exception(f"Failed to process and upload data for {key}", exc_info=True)
