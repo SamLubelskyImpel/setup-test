10333bc1eac4b1ac616f2ae65774c8bbd703c143
Lucas Weber <lucas.builder@impel.ai>
2024-12-15 19:15:07 -0300

    parsing added to get dealer lead status from pbs

--- crm/crm-integrations/pbs/app/get_dealer_lead_statuses.py
+++ crm/crm-integrations/pbs/app/get_dealer_lead_statuses.py
@@ -6,31 +6,25 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-# def parse_employee_data(api_response):
-#     """
-#     Transforms the raw API response to the format required by the OAS.
-#     """
-#     parsed_data = []
+def parse_status_data(api_response):
+    """
+    Transforms the raw API response to be formatted.
+    """
+    parsed_data = []
 
-#     # Extract the 'Employees' list from the API response
-#     employees = api_response.get('Employees', [])
+    response_field = api_response.get('Response', [])
+    status_list = response_field.get('SetupList', [])
 
-#     for employee in employees:
-#         if not employee.get('Sales'):
-#             continue
-#         # Extract and transform relevant fields
-#         parsed_employee = {
-#             "email": [employee.get("EmailAddress", "")],
-#             "first_name": employee.get("FirstName", ""),
-#             "last_name": employee.get("LastName", ""),
-#             "phone": [phone for phone in [employee.get("Phone", ""), employee.get("CellPhone", "")] if phone],
-#             "crm_salesperson_id": employee.get("EmployeeId", ""),
-#             "position_name": employee.get("SalesRole", "")
-#         }
+    for status in status_list:
+        parsed_status = {
+            "Name": status.get("Name", ""),
+            "SystemStatus": status.get("SystemStatus", ""),
+            "Default": status.get("Lead", ""),
+            "Inactive": status.get("Inactive", "")
+        }
+        parsed_data.append(parsed_status)
 
-#         parsed_data.append(parsed_employee)
-
-#     return parsed_data
+    return parsed_data
 
 
 def lambda_handler(event, context):
@@ -44,8 +38,8 @@ def lambda_handler(event, context):
     try:
         result = api.get_dealer_lead_statuses(crm_dealer_id)
         logger.info("Successfully received response from API: %s", result)
-        # parsed_result = parse_employee_data(result)
-        # logger.info("Successfully parsed response from API to: %s", parsed_result)
+        parsed_result = parse_status_data(result)
+        logger.info("Successfully parsed response from API to: %s", parsed_result)
 
     except Exception as e:
         logger.error("Failed to retrieve employee data: %s", e)
@@ -56,7 +50,7 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dumps(result)
+        "body": json.dumps(parsed_result)
     }
 
 
