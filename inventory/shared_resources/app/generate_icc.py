da15f1ff89a6f9271d7df24ee3e47d889c5ada20
clairemurray <cmurray@impel.ai>
2025-01-14 15:54:57 -0500

    Log instead of raising errors when no active partners are found for the generate ICC file lambda

--- inventory/shared_resources/app/generate_icc.py
+++ inventory/shared_resources/app/generate_icc.py
@@ -111,7 +111,8 @@ def lambda_handler(event: Any, context: Any) -> Any:
         active_dips = rds_instance.get_active_dealer_integration_partners(integration_partner)
 
         if not active_dips:
-            raise ValueError("No active dealer integration partners for integration partner")
+            logger.warning("No active dealer integration partners for integration partner")
+            return
 
         for dip, provider_dealer_id in active_dips:
             inv_data = rds_instance.get_on_lot_inventory(dip)
