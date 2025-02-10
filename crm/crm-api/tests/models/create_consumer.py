f44d92d2bbc4a6b504cdd9b8b255c20b88264d61
Konstantine Bakradze <kbakradze@impel.ai>
2025-02-10 16:08:39 +0400

    delete test validation from tests

--- crm/crm-api/tests/models/create_consumer.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel, Field
-
-class CreateConsumerRequest(BaseModel):
-    name: str = Field(..., description="The name of the consumer")
-    email: str = Field(..., regex=r"[^@]+@[^@]+\.[^@]+", description="The email of the consumer")
