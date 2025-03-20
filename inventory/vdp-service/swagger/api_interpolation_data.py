f7596508a20a20dc95c61dc2894921e4f2fd659a
sachdeva-impel <ssachdeva@impel.ai>
2025-03-20 17:00:05 -0400

    Renamed get_vdp to query_vdp

--- inventory/vdp-service/swagger/api_interpolation_data.py
+++ inventory/vdp-service/swagger/api_interpolation_data.py
@@ -13,10 +13,10 @@ x-amazon-apigateway-request-validator: full
 """
 
 # httpMethod should always be POST even for other endpoint types
-GET_VDP_LAMBDA_INFO = """
+QUERY_VDP_LAMBDA_INFO = """
 x-amazon-apigateway-integration:
   uri:
-    Fn::Sub: arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31/functions/${GetVDP.Arn}/invocations
+    Fn::Sub: arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31/functions/${QueryVDP.Arn}/invocations
   passthroughBehavior: never
   httpMethod: POST
   type: aws_proxy
@@ -24,5 +24,5 @@ x-amazon-apigateway-integration:
 
 VDP_API_INTERPOLATION_DATA = {
     "VDP_API_AWS_VALIDATION": VDP_API_AWS_VALIDATION,
-    "GET_VDP_LAMBDA_INFO": GET_VDP_LAMBDA_INFO,
+    "GET_VDP_LAMBDA_INFO": QUERY_VDP_LAMBDA_INFO,
 }
