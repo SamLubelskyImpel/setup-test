cb1fa7477efed9ceaaa9f2749389ae43437ecca3
sachdeva-impel <ssachdeva@impel.ai>
2025-03-20 12:19:42 -0400

    Updated authorizer

---
+++ inventory/vdp-service/swagger/api_interpolation_data.py
@@ -0,0 +1,28 @@
+"""Contains interpolation data for the swagger api template.
+This is data that we do not want to use for the client copy, but need for deployment.
+As such this data is interpolated with the client copy as part of deployment script."""
+
+VDP_API_AWS_VALIDATION = """
+x-amazon-apigateway-request-validators:
+  full:
+    validateRequestBody: true
+    validateRequestParameters: true
+    validateRequestHeaders: true
+
+x-amazon-apigateway-request-validator: full
+"""
+
+# httpMethod should always be POST even for other endpoint types
+GET_VDP_LAMBDA_INFO = """
+x-amazon-apigateway-integration:
+  uri:
+    Fn::Sub: arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31/functions/${GetVDP.Arn}/invocations
+  passthroughBehavior: never
+  httpMethod: POST
+  type: aws_proxy
+"""
+
+VDP_API_INTERPOLATION_DATA = {
+    "VDP_API_AWS_VALIDATION": VDP_API_AWS_VALIDATION,
+    "GET_VDP_LAMBDA_INFO": GET_VDP_LAMBDA_INFO,
+}
