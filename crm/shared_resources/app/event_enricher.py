8cce5ed013b48776452d069c8680d01f8b80d064
Lucas Weber <lucas.builder@impel.ai>
2025-05-13 17:22:20 -0300

    BatchProcessor added to EventEnricher lambda and other minor improvements

--- crm/shared_resources/app/event_enricher.py
+++ crm/shared_resources/app/event_enricher.py
@@ -5,6 +5,13 @@ import boto3
 from utils import call_crm_api
 from json import dumps, loads
 
+from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSRecord
+from aws_lambda_powertools.utilities.batch import (
+    BatchProcessor,
+    EventType,
+    process_partial_response,
+)
+
 ENVIRONMENT = environ.get("ENVIRONMENT", "test")
 CRM_API_SECRET_KEY = environ.get("UPLOAD_SECRET_KEY")
 CRM_API_URL = environ.get("CRM_API_URL")
@@ -14,21 +21,16 @@ secret_client = boto3.client("secretsmanager")
 logger = logging.getLogger()
 
 
-def lambda_handler(event, context):
+def record_handler(record: SQSRecord):
     """
-    This function is triggered by a CRM API event.
-    It processes the event, adds additional information
-    and sends it to an CrmEventBus.
+    Process a single EventEnricher SQS record.
     """
-    logger.info("Received event: %s", event)
+    logger.info(f"Record: {record}")
 
     try:
-        CRM_API_URL = 'https://wpl2oe1li7.execute-api.us-east-1.amazonaws.com/lucas2/'
-        record = event["Records"][0]
         body = loads(record["body"])
         idp_dealer_id = body.get("Detail", {}).get("idp_dealer_id")
-
-        dealer = call_crm_api(f"{CRM_API_URL}dealers/idp_dealer_id/{idp_dealer_id}")
+        dealer = call_crm_api(f"{CRM_API_URL}dealers/idp/{idp_dealer_id}")
 
         if not dealer:
             logger.error(f"Dealer not found for idp_dealer_id: {idp_dealer_id}")
@@ -41,34 +43,27 @@ def lambda_handler(event, context):
         oem_partner = metadata.get("oem_partner") or {}
         oem_partner_name = oem_partner.get("name")
 
-        merged_event = {
-            "detail": {
-                **body,
-                "event_type": [body.get("Detail", {}).get("event_type")],
-                "source_application": [body.get("Detail", {}).get("source_application")],
-                "partner_name": [dealer['integration_partner_name']],
-                "override_partner": [f"{oem_partner_name}"] if oem_partner_name else [],
-            }
-        }
+        body["Detail"]["partner_name"] = dealer['integration_partner_name']
+        body["Detail"]["override_partner"] = f"{oem_partner_name}" if oem_partner_name else None
 
-        logger.info(f"Event to be sent to EventBus: {merged_event}")
+        logger.info(f"Event to be sent to EventBus: {body}")
 
         event_bus_client = boto3.client('events')
         response = event_bus_client.put_events(
             Entries=[
-                {
-                    'Source': 'crm-api',
-                    'DetailType': 'CRM API Event',
-                    'Detail': dumps(merged_event),
-                    'EventBusName': CRM_EVENT_BUS_NAME,
-                },
+                    {
+                        "Source": "EventEnricher",
+                        "DetailType": body["DetailType"],
+                        "Detail": dumps(body["Detail"]),
+                        "EventBusName": body["EventBusName"]
+                    }
             ]
         )
         logger.info(f"Event sent to EventBus: {response}")
 
         return {
             "statusCode": 200,
-            "body": merged_event
+            "body": body
         }
 
     except Exception as e:
@@ -77,3 +72,25 @@ def lambda_handler(event, context):
             "statusCode": 500,
             "body": f"Error processing event: {e}"
         }
+
+
+def lambda_handler(event, context):
+    """
+    This function is triggered by a CRM API event.
+    It processes the event, adds additional information
+    and sends it to an CrmEventBus.
+    """
+    logger.info(f"Event: {event}")
+
+    try:
+        processor = BatchProcessor(event_type=EventType.SQS)
+        result = process_partial_response(
+            event=event,
+            record_handler=record_handler,
+            processor=processor,
+            context=context
+        )
+        return result
+    except Exception as e:
+        logger.error(f"Error processing batch: {e}")
+        raise
