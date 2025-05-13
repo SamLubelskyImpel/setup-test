964e423f81f0e80c42ac601e6f07d3c4ab09f18e
Lucas Weber <lucas.builder@impel.ai>
2025-05-13 17:19:57 -0300

    get_secret removed

--- crm/crm-api/app/common/utils.py
+++ crm/crm-api/app/common/utils.py
@@ -1,26 +1,14 @@
 from os import environ
 import boto3
-from json import dumps, loads
+from json import dumps
 from datetime import datetime
 import logging
 
-sm_client = boto3.client("secretsmanager")
 sqs_client = boto3.client("sqs")
 logger = logging.getLogger()
 
 ENVIRONMENT = environ.get("ENVIRONMENT")
 EVENT_ENRICHER_QUEUE_URL = environ.get("EVENT_ENRICHER_QUEUE_URL")
-
-
-def get_secret(secret_name, secret_key):
-    """Get secret from Secrets Manager."""
-    secret = sm_client.get_secret_value(
-        SecretId=f"{'prod' if ENVIRONMENT == 'prod' else 'test'}/{secret_name}"
-    )
-    secret = loads(secret["SecretString"])[str(secret_key)]
-    secret_data = loads(secret)
-
-    return secret_data
 
 
 def send_message_to_event_enricher(payload_details):
@@ -29,7 +17,7 @@ def send_message_to_event_enricher(payload_details):
     payload = {
                 "Time": datetime.now().isoformat(),
                 "DetailType": "JSON",
-                "EventBusName": f"crm-shared-${ENVIRONMENT}-CrmEventBus",
+                "EventBusName": f"crm-shared-{ENVIRONMENT}-CrmEventBus",
                 "Source": "crm-api",
                 "Detail": payload_details
             }
