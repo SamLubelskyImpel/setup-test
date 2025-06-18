06348bf405899de931cb998ea379e60cad1c3212
Sam Lubelsky <slubelsky@impel.ai>
2025-06-18 14:16:13 -0400

    fixed that issue

--- crm/crm-integrations/carsales-au/app/send_activity.py
+++ crm/crm-integrations/carsales-au/app/send_activity.py
@@ -1,14 +1,14 @@
-from typing import Any
-from os import environ
 from logging import getLogger
+from os import environ
+from typing import Any
+
+from api_wrappers import CarsalesApiWrapper, CRMApiError, CrmApiWrapper
 from aws_lambda_powertools.utilities.batch import (
     BatchProcessor,
     EventType,
     process_partial_response,
 )
 from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSRecord
-
-from api_wrappers import CarsalesApiWrapper, CrmApiWrapper, CRMApiError
 
 CRM_API_DOMAIN = environ.get("CRM_API_DOMAIN")
 CRM_API_SECRET_KEY = environ.get("UPLOAD_SECRET_KEY")
@@ -18,22 +18,19 @@ logger.setLevel(environ.get("LOGLEVEL", "INFO").upper())
 
 crm_api = CrmApiWrapper()
 
+
 def record_handler(record: SQSRecord):
     """Create activity on Carsales."""
     logger.info(f"Record: {record}")
-    here are some changes that dont make sense
-    and here are some more
-    lets try again
-    more
     try:
         body = record.json_body
         details = body.get("detail", {})
-        
+
         activity = crm_api.get_activity(details["activity_id"])
 
         if not activity:
             logger.warning(f"Activity not found for ID: {details['activity_id']}")
-        
+
         logger.info(f"Activity: {activity}")
 
         carsales_crm_api = CarsalesApiWrapper(activity=activity)
@@ -41,14 +38,17 @@ def record_handler(record: SQSRecord):
     except CRMApiError as e:
         logger.error(f"CRM API Error: {e}")
         raise
-    except Exception as e:
-        logger.exception(f"Failed to post activity {activity['activity_id']} to Carsales")
-        logger.error(f"[SUPPORT ALERT] Failed to Send Activity [CONTENT]."
-                     f"LeadId: {details['lead_id']}"
-                     f"CRMLeadId: {activity['crm_lead_id']}"
-                     f"DealerIntegrationPartnerId: {details['idp_dealer_id']}"
-                     f"ActivityType: {activity['activity_type']}"
-                     f"ActivityId: {activity['activity_id']}"
+    except Exception:
+        logger.exception(
+            f"Failed to post activity {activity['activity_id']} to Carsales"
+        )
+        logger.error(
+            f"[SUPPORT ALERT] Failed to Send Activity [CONTENT]."
+            f"LeadId: {details['lead_id']}"
+            f"CRMLeadId: {activity['crm_lead_id']}"
+            f"DealerIntegrationPartnerId: {details['idp_dealer_id']}"
+            f"ActivityType: {activity['activity_type']}"
+            f"ActivityId: {activity['activity_id']}"
         )
         raise
 
