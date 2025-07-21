5ebef0abf7433bc9cbeb4f72c4c0d0957a7bb203
Sam Lubelsky <slublesky@impel.ai>
2025-07-21 09:47:13 -0400

    added octopus script

---
+++ .octopus/scripts/deploy_release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ls
+pwd
+cd crm/crm-integrations/dealerpeak
+
+AWS_REGION=$(get_octopusvariable "AWS_REGION")
+echo "AWS_REGION: $AWS_REGION"
+
+sam build --parallel
+sam deploy --config-env "test" --no-confirm-changeset --no-fail-on-empty-changeset --no-progressbar --s3-bucket=spincar-deploy-${AWS_REGION}
