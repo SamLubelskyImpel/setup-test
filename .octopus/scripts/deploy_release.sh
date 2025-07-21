9f8293d387db60961cddda37334be14f696bb8f5
Sam Lubelsky <slublesky@impel.ai>
2025-07-21 10:19:28 -0400

    changed deploy_release

--- .octopus/scripts/deploy_release.sh
+++ .octopus/scripts/deploy_release.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-ls
 pwd
-cd ../../crm/crm-integrations/dealerpeak
+ls
+cd ../..
+pwd
+ls
+cd crm/crm-integrations/dealerpeak
+pwd
 
 AWS_REGION=$(get_octopusvariable "AWS_REGION")
 echo "AWS_REGION: $AWS_REGION"
+
+curl -LJO https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+chmod +x sam-installation/install
+./sam-installation/install
+sam --version
 
 sam build --parallel
 sam deploy --config-env "test" --no-confirm-changeset --no-fail-on-empty-changeset --no-progressbar --s3-bucket=spincar-deploy-${AWS_REGION}
