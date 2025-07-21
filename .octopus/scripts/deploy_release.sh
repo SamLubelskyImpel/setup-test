8a28059121f90e789bfebe4b91e8b306c9ce490a
Sam Lubelsky <slublesky@impel.ai>
2025-07-21 16:15:49 -0400

    script updated

--- .octopus/scripts/deploy_release.sh
+++ .octopus/scripts/deploy_release.sh
@@ -4,9 +4,20 @@ echo "cding to dealerpeak directory"
 cd ../..
 cd crm/crm-integrations/dealerpeak
 
-
+AWS_ACCESS_KEY_ID=$(get_octopusvariable "AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY=$(get_octopusvariable "AWS_SECRET_ACCESS_KEY")
 AWS_REGION=$(get_octopusvariable "AWS_REGION")
+ROLE_ARN=$(get_octopusvariable "ROLE_ARN")
+
 echo "AWS_REGION: $AWS_REGION"
+
+aws configure set profile.default.output json
+aws configure set profile.default.region $AWS_REGION
+
+aws configure set profile.unified-test.role_arn $ROLE_ARN
+aws configure set profile.unified-test.source_profile default
+aws configure set profile.unified-test.region $AWS_REGION
+aws configure set profile.unified-test.output json
 
 echo "building and deploying"
 sam build --parallel
